f4e76739cea56c7405b0ef23f54773502ba407b1
rz-p <pramadhan9@gmail.com>
2022-09-24 19:13:09 +0200

    init project

---
+++ src/main/java/com/ijoapp/ijoapp/IjoAppApplication.java
@@ -0,0 +1,13 @@
+package com.ijoapp.ijoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IjoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ijoapp/ijoapp/IjoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ijoapp.ijoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IjoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
